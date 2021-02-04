c087dade477752dd6f08e7c523ab642ee29e8874
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2021-02-04 17:16:33 -0600

    Setup basic controller for conversions

--- src/main/java/com/example/cookingmeasurementsconverter/Controllers/ConversionsController.java
+++ src/main/java/com/example/cookingmeasurementsconverter/Controllers/ConversionsController.java
@@ -1,21 +1,28 @@
 package com.example.cookingmeasurementsconverter.Controllers;
 
 import com.example.cookingmeasurementsconverter.Services.ConversionsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ConversionsController {
+    @Autowired
     private ConversionsService conversionsService;
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+//        model.addAttribute("results");
         return "home";
     }
 
     @GetMapping("/convert")
-    public String conversions(){
+    public String conversions(@RequestParam(value = "firstUnit") String firstUnit,
+                              @RequestParam(value = "firstAmount") double firstAmount,
+                              @RequestParam(value = "secondUnit") String secondUnit,
+                              Model model){
+        model.addAttribute("results", conversionsService.selector(firstUnit, firstAmount, secondUnit));
         return "home";
     }
 }
