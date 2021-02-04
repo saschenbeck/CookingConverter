f5485bf5e5bfbad9c4af36ef5efb5f4da068b43f
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2021-02-04 17:17:40 -0600

    Setup tests for teaspoons to tablespoons

---
+++ src/test/java/com/example/cookingmeasurementsconverter/ConverterTest/ConverterTest.java
@@ -0,0 +1,22 @@
+package com.example.cookingmeasurementsconverter.ConverterTest;
+import static org.junit.Assert.*;
+import com.example.cookingmeasurementsconverter.Services.ConversionsService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConverterTest {
+    @Autowired
+    private ConversionsService conversionsService;
+
+
+    @Test
+    public void teaspoonsToTablespoonsEquals(){
+        System.out.println("Test Start");
+        double actual = conversionsService.tspToTbsp(3);
+        assertEquals(1.0, actual, 0);
+        System.out.println("Test complete");
+    }
+}
